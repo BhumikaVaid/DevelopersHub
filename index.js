5eb6bd31fe7b9fcd17b78f70cabf1d94efeefec2
Bhumika Vaid <bhumikavaid.1711@gmail.com>
2023-04-30 12:59:36 -0400

    committed

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const http = require('http');
 const path = require('path');
 const fs = require('fs');
 
-const server = http.createServer((req,res)=>{
+//const cors = require('cors');
 
+const server = http.createServer((req,res)=>{
 
     console.log(req.url);
    
@@ -11,32 +12,37 @@ const server = http.createServer((req,res)=>{
 
     if(req.url ==='/'){
         
-        fs.readFile(path.join(__dirname,'public','index.html'), (err, content)=>{
+        fs.readFile(path.join(__dirname,'index.html'), (err, content)=>{
             if (err) throw err;
             res.writeHead(200,{'Content-Type':'text/html'});
+            // res.setHeader('Access-Control-Allow-Origin', '*');
+            // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
             res.end(content);
 
         });
     }
     else if(req.url ==='/api'){
 
-        fs.readFile(path.join(__dirname,'public','db.json'),'utf-8',(err, content)=>{
+        fs.readFile(path.join(__dirname,'db.json'),'utf-8',(err, content)=>{
 
             if (err) throw err;
             res.writeHead(200, {'Content-Type':'application/json'});
+            // res.setHeader('Access-Control-Allow-Origin', '*');
+            // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
             res.end(content);
-        })
+        });
 
     
     }
     else{
 
         res.writeHead(404,{'Content-Type':'text/html'});
-        res.end(" <h1> 404 Nothing Found </h1>")
+        res.end("<h1> 404 Nothing Found </h1>");
     }
 
 
 
 })
 
-server.listen(9000, ()=> console.log("Our server is running"));
+server.listen(5050, ()=> console.log("Our server is running"));
+
