89596d574a7fc1d1cfe7420d3a53d5eee36cf0be
Bhumika Vaid <bhumikavaid.1711@gmail.com>
2023-05-01 17:28:31 -0400

    first commit

--- index.js
+++ index.js
@@ -2,43 +2,62 @@ const http = require('http');
 const path = require('path');
 const fs = require('fs');
 
-//const cors = require('cors');
+const { MongoClient} = require('mongodb');
+const uri = "mongodb+srv://bhumika:vaid1709@cluster0.ceowmum.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
 
-const server = http.createServer((req,res)=>{
+const cors = require('cors');
+
+const server = http.createServer(async (req,res)=>{
 
     console.log(req.url);
-   
-
 
     if(req.url ==='/'){
         
-        fs.readFile(path.join(__dirname,'index.html'), (err, content)=>{
+        fs.readFile(path.join(__dirname,'public','index.html'), (err, content)=>{
             if (err) throw err;
             res.writeHead(200,{'Content-Type':'text/html'});
-            // res.setHeader('Access-Control-Allow-Origin', '*');
-            // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
             res.end(content);
 
         });
     }
-    else if(req.url ==='/api'){
-
-        fs.readFile(path.join(__dirname,'db.json'),'utf-8',(err, content)=>{
-
-            if (err) throw err;
-            res.writeHead(200, {'Content-Type':'application/json'});
-            // res.setHeader('Access-Control-Allow-Origin', '*');
-            // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-            res.end(content);
-        });
-
     
-    }
-    else{
+    else if(req.url ==='/api' && req.method==='GET'){
+       
+            
+            try {
+                
+                await client.connect();
 
-        res.writeHead(404,{'Content-Type':'text/html'});
-        res.end("<h1> 404 Nothing Found </h1>");
-    }
+                const cursor = client.db("DevelopersHub").collection("DevelopersCollection").find({});
+                const result = await cursor.toArray();
+                console.log(result);
+
+                res.setHeader('Access-Control-Allow-Origin', '*');
+                res.setHeader('Content-Type','application/json');
+                // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+                res.end(JSON.stringify(result));
+                   
+            }
+            catch(e){
+
+                await console.log(e);
+
+
+            } finally {
+              
+                await client.close();
+                console.log("Connection closed!")
+            }
+     
+    
+        }
+        else{
+
+            res.writeHead(404,{'Content-Type':'text/html'});
+            res.end("<h1> 404 Nothing Found </h1>");
+        }
 
 
 
